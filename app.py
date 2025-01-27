457a8468d4976af723b7bc788d4be7a0ccabcca6
Suleyman-2010 <sulikosp2010@gmail.com>
2025-01-28 00:04:02 +0400

    Mostly finished the user page and changed some things in python.

--- app.py
+++ app.py
@@ -1,39 +1,57 @@
-from flask import Flask, render_template, redirect, url_for, request
-from user import User
+from flask import Flask, render_template, redirect, request
+from werkzeug import Response
+
+from classes import User, Login
 
 app: Flask = Flask(__name__)
+# app.debug = True
 current_user: User | None = None
 users: list[User] = list()
 
 
 @app.route("/")
 def home():
-    return redirect(url_for("login"))
+    if current_user is None:
+        return redirect("login")
+    else:
+        return render_template("user.html", user=current_user)
 
 
 @app.route("/signup", methods=["POST", "GET"])
-def signup():
-    if request.method == "POST":
-        username: str = request.form["username"]
-        password: str = request.form["password"]
-        email: str = request.form["email"]
-        gender: str = request.form["gender"]
-        users.append(User(username, password, email, gender))
-        current_user = users[-1]
-        return redirect(url_for('user'))
-    return render_template("signup.html")
+def signup() -> str | Response:
+    global current_user
+    if current_user is None:
+        if request.method == "POST":
+            username: str = request.form["username"]
+            password: str = request.form["password"]
+            email: str = request.form["email"]
+            gender: str = request.form["gender"]
+            users.append(User(username, password, email, gender))
+            current_user = users[-1]
+            return render_template("user.html", user=current_user)
+        return render_template("signup.html", user=current_user)
+    else:
+        return redirect("/")
 
 
 @app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == 'POST':
-        username: str = request.form['username']
-        password: str = request.form['password']
-    return render_template("login.html")
+def login() -> str | Response:
+    if current_user is None:
+        if request.method == "POST":
+            username: str = request.form["username"]
+            password: str = request.form["password"]
+            login_object: Login = Login(username, password)
+            for user in users:
+                if user == login_object:
+                    return render_template("user.html", user=user)
+        return render_template("login.html", user=current_user)
+    else:
+        return redirect("/")
 
-@app.route('/user')
+
+@app.route("/user")
 def userinfo():
-    return render_template('user.html', user=current_user)
+    return render_template("user.html", user=current_user)
 
 
 if __name__ == "__main__":
---
+++ classes.py
@@ -0,0 +1,15 @@
+class User:
+    def __init__(self, username: str, password: str, email: str, gender: str):
+        self.username = username
+        self.password = password
+        self.email = email
+        self.gender = gender
+
+    def eq(self, other):
+        return self.username == other.username and self.password == other.password
+
+
+class Login:
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
